780d8349a645da592d605b5344b5e6642b141f17
SangHun Lee <sanghunlee1333@gmail.com>
2025-08-17 04:22:04 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 73.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+            
+        answer = Integer.toString(Integer.parseInt(n_str));
+        
+        return answer; 
+    }
+}
