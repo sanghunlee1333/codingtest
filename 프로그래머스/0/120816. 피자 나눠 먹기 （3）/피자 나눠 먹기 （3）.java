763c009037afabcaae0ae758075535ee1a05da6a
SangHun Lee <sanghunlee1333@gmail.com>
2025-08-30 12:10:29 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.04 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int slice, int n) {
+        int answer = 0;
+        answer = n % slice != 0 ? n / slice + 1 : n / slice;
+        
+        return answer;
+    }
+}
