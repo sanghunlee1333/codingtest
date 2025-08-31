6357d76f24112cfb6033a0a3906caebe805354cf
SangHun Lee <sanghunlee1333@gmail.com>
2025-08-31 11:47:30 +0900

    [level 0] Title: 주사위의 개수, Time: 0.02 ms, Memory: 80.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] box, int n) {
+        int answer = 1;
+        
+        for(int len : box) {
+            answer *= len / n;
+        }
+        
+        return answer;
+    }
+}
