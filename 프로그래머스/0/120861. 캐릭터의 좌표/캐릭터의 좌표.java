a8bbc1789ee30528fec5c51ec537835370e498f7
SangHun Lee <sanghunlee1333@gmail.com>
2025-09-09 11:10:42 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.03 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] answer = {};
+        answer = new int[2];
+        
+        int max_width = (board[0]) / 2;
+        int max_height = (board[1]) / 2;
+        
+        for(int i = 0; i < keyinput.length; i++) {            
+            if(keyinput[i].equals("left") && answer[0] > -max_width) {
+                answer[0] -= 1;
+            }
+            else if(keyinput[i].equals("right") && answer[0] < max_width) {
+                answer[0] += 1;
+            }
+            else if(keyinput[i].equals("up") && answer[1] < max_height) {
+                answer[1] += 1;
+            }
+            else if(keyinput[i].equals("down") && answer[1] > -max_height) {
+                answer[1] -= 1;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
